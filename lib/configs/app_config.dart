51836e4ef68bc364736f47faf68c49d085708467
HuntsmanIQ <MohammedAlhili777@gmail.com>
2025-08-25 18:09:00 +0300

    release

--- lib/configs/app_config.dart
+++ lib/configs/app_config.dart
@@ -2,7 +2,7 @@ class AppConfig {
   static String appName = "Asnank"; //Rename it with your app name
 
   static const bool https =
-      false; //Make it true if your domain support https otherwise make it false
+      true; //Make it true if your domain support https otherwise make it false
 
   // static const domain = "192.168.31.237/enmart-laravel"; //If you want to connect with local host provide your ip address instead localhost.
   static const domain =
